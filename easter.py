6c6245baa0beabb6a9d0a46e580fff80fa30fc38
jetsten <jetsten@jetsten-macbookpro.local>
2016-09-06 13:43:55 -0400

    made mistake on april date

--- easter.py
+++ easter.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 def main():
     
-    n=year("Please Enter a year: ")
+    n=input("Please Enter a year: ")
     na = n%19
     nb = n%4
     nc = n%7
     nd = ((19*na)+24)%30
     ne = ((2*nb)+(4*nc)+(6*nd)+5)%7
     easter = 22+nd+ne
-    print "In The Year of the Lord",year,", Easter is on",
-    if days <= 31:
+    print "In The Year of the Lord",n,", Easter is on",
+    if easter <= 31:
         print"March " ,str(easter)
     else:
-        print"April " , str(easter)
+        print"April " , str(easter-31)
  
 main()
